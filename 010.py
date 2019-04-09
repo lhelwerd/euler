a9477ca86eaf09698340282c245d96374d519ef5
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-04-09 17:54:13 +0200

    Calculate #10 using Sieve

---
+++ 010.py
@@ -0,0 +1,20 @@
+"""
+PROBLEM:     010
+AUTHOR:      Leon Helwerda
+STATUS:      needs-optimization
+INTERPRETER: Python 2 or 3
+EXPLANATION:
+    Sum of primes below two million
+"""
+
+import timeit
+from Euler import Sieve
+
+def problem():
+    primes = Sieve(int(2e6))
+    print(sum(primes))
+
+if __name__== "__main__":
+    print(timeit.timeit("problem()", setup="from __main__ import problem",
+                        number=1))
+
