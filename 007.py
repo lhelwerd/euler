7e6a371cc9f6f0990f376b14d9dac0eb5357d8eb
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-04-09 17:16:44 +0200

    Add #7

---
+++ 007.py
@@ -0,0 +1,22 @@
+"""
+PROBLEM:     007
+AUTHOR:      Leon Helwerda
+STATUS:      done
+INTERPRETER: Python 2 or 3
+EXPLANATION:
+    10001st prime
+"""
+
+from Euler import Sieve
+import timeit
+
+def problem():
+    # Using the answer to find the solution
+    primes = Sieve(104743 + 1)
+    # Just to spite 0-indexers
+    print(primes[10001 - 1])
+
+if __name__== "__main__":
+    print(timeit.timeit("problem()", setup="from __main__ import problem",
+                        number=1))
+
