1092753185fcc268405d8bda301895f52edf6f52
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-04-12 22:25:15 +0200

    Finish #35

---
+++ 035.py
@@ -0,0 +1,29 @@
+"""
+PROBLEM:     035
+AUTHOR:      Leon Helwerda
+STATUS:      done
+INTERPRETER: Python 2 or 3
+EXPLANATION:
+    Number of prime numbers whose digit permutations are themselves prime
+"""
+
+from itertools import permutations
+import timeit
+from Euler import Sieve
+
+def problem():
+    primes = Sieve(int(1e6))
+    circular = set()
+    for prime in primes:
+        digits = str(prime)
+        test = digits[1:] + digits[0]
+        while test != digits and int(test) in primes:
+            test = test[1:] + test[0]
+        if test == digits:
+            circular.add(prime)
+
+    print(len(circular))
+
+if __name__== "__main__":
+    print(timeit.timeit("problem()", setup="from __main__ import problem",
+                        number=1))
