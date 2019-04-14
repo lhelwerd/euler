cba4211f935216efc1cf99d040526fbdf26c8b5a
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-04-14 20:56:01 +0200

    Finish #47

---
+++ 047.py
@@ -0,0 +1,26 @@
+"""
+PROBLEM:     047
+AUTHOR:      Leon Helwerda
+STATUS:      done
+INTERPRETER: Python 2 or 3
+EXPLANATION:
+    Smallest odd non-prime number that cannot be expressed as the sum of
+    a prime number plus two times a square number.
+"""
+
+from builtins import range
+import timeit
+from Euler.prime import PrimeSet
+
+def problem():
+    primes = PrimeSet(10**3)
+    i = 35
+    double_squares = [2 * x ** 2 for x in range(1, 55)]
+    while i in primes or any(i - s in primes for s in double_squares if s < i):
+        i += 2
+
+    print(i)
+
+if __name__== "__main__":
+    print(timeit.timeit("problem()", setup="from __main__ import problem",
+                        number=1))
