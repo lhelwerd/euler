bd483678df50f9d0ebb2205127a94b8ab983858e
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-04-26 11:46:47 +0200

    Finish #82

--- 082.py
+++ 082.py
@@ -1,11 +1,11 @@
 """
-PROBLEM:     081
+PROBLEM:     082
 AUTHOR:      Leon Helwerda
 STATUS:      done
 INTERPRETER: Python 2 or 3
 EXPLANATION:
-    Minimal path sum in a matrix of paths from top left to bottom right that
-    only move right or down between the cells of the matrix.
+    Minimal path sum in a matrix of paths between the leftmost and rightmost
+    columns that move right, down or up.
 """
 
 from builtins import range
@@ -13,7 +13,7 @@ import timeit
 
 def problem():
     matrix = []
-    with open('p081_matrix.txt') as f:
+    with open('p082_matrix.txt') as f:
         for line in f:
             matrix.append([int(c) for c in line.rstrip().split(',')])
 
@@ -22,19 +22,23 @@ def problem():
     sums = []
     for i in range(n):
         sums.append([0] * n)
+        sums[i][0] = matrix[i][0]
 
-    sums[0][0] = matrix[0][0]
-    for i in range(1, n):
-        sums[i][0] = sums[i-1][0] + matrix[i][0]
     for j in range(1, n):
         sums[0][j] = sums[0][j-1] + matrix[0][j]
-
-    for i in range(1, n):
-        for j in range(1, n):
+        for i in range(1, n):
+            # Only take paths from above or left into account
             sums[i][j] = min(sums[i-1][j], sums[i][j-1]) + matrix[i][j]
 
-    print(sums[n-1][n-1])
+            # Update paths higher in this column if our path is better
+            k = i - 1
+            while k >= 0 and sums[k][j] > sums[k+1][j] + matrix[k][j]:
+                sums[k][j] = sums[k+1][j] + matrix[k][j]
+                k -= 1
+
+    print(min(sums[i][n-1] for i in range(n)))
 
 if __name__ == "__main__":
     print(timeit.timeit("problem()", setup="from __main__ import problem",
                         number=1))
+
