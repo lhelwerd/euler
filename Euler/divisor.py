6a7d4c3823c484fe7eb37590563de692e3dcfe7b
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-04-30 10:06:22 +0200

    Improve prime set and divisors/totients
    - Include Miller test for primality
    - Allow Miller test for prime set extension
    - Miller-aware totient function with formula speedups

--- Euler/divisor.py
+++ Euler/divisor.py
@@ -1,10 +1,10 @@
 from builtins import range
 
-def proper_divisors(n):
+def proper_divisors(n, add=1):
     # Find proper divisors of `n`
     divisors = set()
-    divisors.add(1)
-    for i in range(2, n // 2):
+    divisors.add(add)
+    for i in range(2, n // 2 + 1):
         if n % i == 0:
             divisors.add(i)
             divisors.add(n // i)
