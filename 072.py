fe268f8287896a23432baf7ddd1f867777d3a8e1
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-05-02 11:54:59 +0200

    Finish #72

---
+++ 072.py
@@ -0,0 +1,26 @@
+"""
+PROBLEM:     072
+AUTHOR:      Leon Helwerda
+STATUS:      done
+INTERPRETER: Python 2 or 3
+EXPLANATION:
+    Number of reduced proper fraction with denominator below one million.
+"""
+
+from past.builtins import xrange as range
+import timeit
+from Euler.prime import PrimeSet
+
+def problem():
+    limit = 10**6
+    primes = PrimeSet(limit)
+    phis = [0, 1]
+    for d in range(2, limit + 1):
+        phi = primes.totient(d, phis)
+
+    # Do not count 1/1
+    print(sum(phis) - 1)
+
+if __name__ == "__main__":
+    print(timeit.timeit("problem()", setup="from __main__ import problem",
+                        number=1))
