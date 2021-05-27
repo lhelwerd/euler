ad320c8327605888e22d8e87734f1137f6c11e3d
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2021-05-27 18:04:00 +0200

    Improve palindrome functions
    - Document function parameters
    - Correct when limit is an odd-power of ten (i.e., 1000 = 1e3)

--- Euler/palindrome.py
+++ Euler/palindrome.py
@@ -2,13 +2,25 @@ from builtins import range
 import math
 
 def generate(limit):
-    for part in range(1, limit / 10**(math.log10(limit) / 2)):
+    """
+    Generator providing palindromes. Palindromes are not provided in any
+    specific order. Limit should be a power of ten.
+    """
+
+    length = int(math.log10(limit))
+
+    for part in range(1, int(limit / 10**int(math.log10(limit) / 2))):
         digits = str(part)
-        yield int(digits + digits[::-1])
+        if len(digits) < length - 1:
+            yield int(digits + digits[::-1])
         if len(digits) >= 2:
             yield int(digits + digits[len(digits) - 2::-1])
         else:
             yield part
 
 def is_palindrome(digits):
+    """
+    Check if the provided string of digits is a palindrome.
+    """
+
     return digits == digits[::-1]
