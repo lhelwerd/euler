6dbf757b695e1440fb3ba191f96c43225957a716
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2025-11-01 19:50:15 +0100

    Address issues in text module

--- Euler/text.py
+++ Euler/text.py
@@ -7,10 +7,51 @@ from os import PathLike
 from pathlib import Path
 
 
+def _handle_buffer(
+    buffer: str,
+    result: list[str],
+    reducer: Callable[[list[str], str], None] = (
+        lambda result, word: result.append(word)
+    ),
+    word: str = "",
+    in_word: bool = False,
+) -> tuple[str, bool]:
+    j = 0
+    i = buffer.find('"')
+    while i != -1:
+        if in_word:
+            word += buffer[j:i]
+
+        if word != "":
+            reducer(result, word)
+            in_word = False
+        else:
+            in_word = j == 0
+            j = i + 1
+
+        if not in_word:
+            # Next open
+            i = buffer.find('"', i + 1)
+            j = i + 1
+            in_word = i != -1
+
+        if i != -1:
+            # Next close
+            i = buffer.find('"', i + 1)
+
+        word = ""
+
+    if j > 0:
+        word = buffer[j:]
+
+    return word, in_word
+
+
 def read(
     filename: str | PathLike[str],
-    reducer: Callable[[list[str], str], None] = lambda result,
-    word: result.append(word),
+    reducer: Callable[[list[str], str], None] = (
+        lambda result, word: result.append(word)
+    ),
 ) -> list[str]:
     """
     Read the (potentially large) file containing words between double quotes.
@@ -27,32 +68,8 @@ def read(
     with Path(filename).open("r", encoding="utf-8") as words:
         while buffered:
             buffered = words.read(1024)
-            j = 0
-            i = buffered.find('"')
-            while i != -1:
-                if in_word:
-                    word += buffered[j:i]
-
-                if word != "":
-                    reducer(result, word)
-                    in_word = False
-                else:
-                    in_word = j == 0
-                    j = i + 1
-
-                if not in_word:
-                    # Next open
-                    i = buffered.find('"', i + 1)
-                    j = i + 1
-                    in_word = i != -1
-
-                if i != -1:
-                    # Next close
-                    i = buffered.find('"', i + 1)
-
-                word = ""
-
-            if j > 0:
-                word = buffered[j:]
+            word, in_word = _handle_buffer(
+                buffered, result, reducer, word, in_word
+            )
 
     return result
