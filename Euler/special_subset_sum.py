d4b09a47c88ad06fdbcd6612fdc54aa843f8cf4d
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2025-11-01 20:07:23 +0100

    Address Sonar issue

--- Euler/special_subset_sum.py
+++ Euler/special_subset_sum.py
@@ -6,6 +6,12 @@ import itertools
 from collections.abc import Collection
 
 
+def _special_subset_sum(comb: Collection[int], comb2: Collection[int]) -> bool:
+    if sum(comb) == sum(comb2):
+        return False
+    return not (len(comb) > len(comb2) and sum(comb) < sum(comb2))
+
+
 def special_subset_sum_set(candidate: Collection[int]) -> bool:
     """
     Test if a candidate set conforms to the special subset sum property.
@@ -17,9 +23,7 @@ def special_subset_sum_set(candidate: Collection[int]) -> bool:
             rest = candidate - set(comb)
             for rest_length in range(1, len(rest) + 1):
                 for comb2 in itertools.combinations(rest, rest_length):
-                    if sum(comb) == sum(comb2):
-                        return False
-                    if len(comb) > len(comb2) and sum(comb) < sum(comb2):
+                    if not _special_subset_sum(comb, comb2):
                         return False
 
     return True
